ed4cbd5bd2ff4a448d0f17a83a740966e4991657
Aychir <Aychir@github.com>
2018-10-02 18:29:32 -0400

    Initial work on login and signup activities.

---
+++ app/src/main/java/osu/edu/cashout/SignupActivity.java
@@ -0,0 +1,17 @@
+package osu.edu.cashout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        private FirebaseAuth mAuth;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+}
