9364934253c47a4463fa574ad56b09f3bf77dc5e
tehGEG <connorcimino1@gmail.com>
2018-10-19 16:22:32 -0400

    U in CRUD

--- app/src/main/java/osu/edu/cashout/Fragments/AccountFragment.java
+++ app/src/main/java/osu/edu/cashout/Fragments/AccountFragment.java
@@ -4,12 +4,16 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,13 +21,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import osu.edu.cashout.R;
+import osu.edu.cashout.User;
 
 public class AccountFragment extends Fragment implements View.OnClickListener{
     private FirebaseAuth mUserAuth;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
+    private FirebaseFirestore mFirestore;
     private Button mUpdateAccountButton;
     private Button mDeleteAccountButton;
     private EditText mFirstName;
@@ -31,6 +40,8 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
     private EditText mUsername;
     private EditText mEmailField;
     private EditText mPasswordField;
+    private EditText mConfirmPasswordField;
+    private User curUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,6 +55,7 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
         mUsername = v.findViewById(R.id.username);
         mEmailField = v.findViewById(R.id.email);
         mPasswordField = v.findViewById(R.id.password);
+        mConfirmPasswordField = v.findViewById(R.id.confirm_password);
 
         //Set on click listeners for the buttons
         mUpdateAccountButton.setOnClickListener(this);
@@ -53,9 +65,20 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
         mUserAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference();
+        mFirestore = FirebaseFirestore.getInstance();
         FirebaseUser currentUser = mUserAuth.getCurrentUser();
         if(currentUser != null) {
-            mEmailField.setText(currentUser.getEmail());
+            DocumentReference userRef = mFirestore.collection("users").document(currentUser.getUid());
+            userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    curUser = documentSnapshot.toObject(User.class);
+                    mFirstName.setText(curUser.getFirstName());
+                    mLastName.setText(curUser.getLastName());
+                    mUsername.setText(curUser.getUsername());
+                    mEmailField.setText(curUser.getEmail());
+                }
+            });
         }
 
         //Read info from the database to display to the user
@@ -79,12 +102,54 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
         int id = v.getId();
         switch (id){
             case R.id.update_button:
-                String firstName = mFirstName.getText().toString();
-                String lastName = mLastName.getText().toString();
-                String username = mUsername.getText().toString();
-                String email = mEmailField.getText().toString();
-                String password = mPasswordField.getText().toString();
+                if(!validateForms()){
+                    return;
+                }
+                else {
+                    curUser.setEmail(mEmailField.getText().toString());
+                    curUser.setFirstName(mFirstName.getText().toString());
+                    curUser.setLastName(mLastName.getText().toString());
+                    curUser.setUsername(mUsername.getText().toString());
+                    //May want to add some checks and/or listeners and/or toasts
+                    mUserAuth.getCurrentUser().updateEmail(curUser.getEmail());
+                    if(!mPasswordField.getText().toString().isEmpty()) {
+                        mUserAuth.getCurrentUser().updatePassword(mPasswordField.getText().toString());
+                    }
+                    mFirestore.collection("users").document(mUserAuth.getCurrentUser().getUid()).set(curUser);
+                }
         }
 
+    }
+
+    private boolean validateForms() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (email.isEmpty()) {
+            mEmailField.setError("An email is required.");
+            valid = false;
+        }
+        //Check the format of the email address
+        else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            mEmailField.setError("Please enter a valid email.");
+            valid = false;
+        }
+
+        String password = mPasswordField.getText().toString();
+        if(!password.isEmpty() && password.length() < 6){
+            mEmailField.setError("Your password must be at least 6 characters long.");
+            valid = false;
+        }
+
+        String passwordConfirmation = mConfirmPasswordField.getText().toString();
+        if (!password.isEmpty() && passwordConfirmation.isEmpty()) {
+            mConfirmPasswordField.setError("You must confirm your password.");
+            valid = false;
+        }
+        else if(!passwordConfirmation.equals(password)){
+            mConfirmPasswordField.setError("Your password entries did not match, please try again.");
+            valid = false;
+        }
+        return valid;
     }
 }
