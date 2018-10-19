ddfa2297c7bdc6b66926a271d2a35b07bc732cfd
Aychir <Aychir@github.com>
2018-10-19 10:33:20 -0400

    Implementation of the User class model.

---
+++ app/src/main/java/osu/edu/cashout/User.java
@@ -0,0 +1,48 @@
+package osu.edu.cashout;
+
+public class User {
+
+    private String mEmail;
+    private String mFirstName;
+    private String mLastName;
+    private String mUsername;
+
+    //Note: We obviously do not want to store password of the user in this database
+    //  Also date of birth will have to wait, if we implement it at all
+
+    public User(){
+
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String mFirstName) {
+        this.mFirstName = mFirstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String mLastName) {
+        this.mLastName = mLastName;
+    }
+
+    public String getUsername() {
+        return mUsername;
+    }
+
+    public void setUsername(String mUsername) {
+        this.mUsername = mUsername;
+    }
+}
