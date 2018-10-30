1b201fb586d959274826f9c3d32aed1849bf5aee
Aychir <Aychir@github.com>
2018-10-30 12:24:58 -0400

    More progress on handling scanning results.

--- app/src/main/java/osu/edu/cashout/AsyncFindProduct.java
+++ app/src/main/java/osu/edu/cashout/AsyncFindProduct.java
@@ -1,142 +1,163 @@
-package osu.edu.cashout;
+//package osu.edu.cashout;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
+//import android.os.AsyncTask;
+//import android.support.v4.app.FragmentActivity;
+//import android.util.Log;
+//
+//
+//
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//
+//import javax.net.ssl.HttpsURLConnection;
+//
+//import osu.edu.cashout.activities.ScanActivity;
+//import osu.edu.cashout.dataModels.Product;
+//
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.util.JsonReader;
-import android.util.Log;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonParser;
-
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import osu.edu.cashout.dataModels.Product;
-
-
-public class AsyncFindProduct extends AsyncTask<String, Void, Product> {
-
-    private static final String TAG = "AsyncFindProduct";
-    private static final String REQUEST_METHOD = "GET";
-    private static final int READ_TIMEOUT = 15000;
-    private static final int CONNECTION_TIMEOUT = 15000;
-
-    //Maybe implement something in onPreExecute to tell the user we are searching
-
-    protected Product doInBackground(String... params){
-
-        Product product = new Product();
-
-        try{
-            //Create the connection to the url to upcitemdb first
-
-            URL upcItemDbUrl = new URL("https://api.upcitemdb.com/prod/trial/lookup?upc=" + params[0]);
-            HttpsURLConnection connection = (HttpsURLConnection) upcItemDbUrl.openConnection();
-
-            connection.setRequestMethod(REQUEST_METHOD);
-            connection.setReadTimeout(READ_TIMEOUT);
-            connection.setConnectTimeout(CONNECTION_TIMEOUT);
-
-            connection.connect();
-
-            if(connection.getResponseCode() == 200){
-                Log.v(TAG, "Successful");
-            }
-            else{
-                Log.v(TAG, "RIP");
-            }
-            //Create a new InputStreamReader
-            InputStream streamReader = (connection.getInputStream());
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String content = br.readLine();
-            System.out.println(content);
-
-            JSONObject object = new JSONObject(content);
-            Log.v(TAG, object.getString("items"));
-
-            //We will assume that the first item in the list is accurate for now
-            JSONArray array = object.getJSONArray("items");
-            Log.v(TAG, array.getJSONObject(0).getString("title"));
-
-            //Set the item from the array of results
-            JSONObject firstItem = array.getJSONObject(0);
-
-            //Set the upc of the product
-            product.setUpc(params[0]);
-
-            //Set the name of the product
-            if(firstItem.getString("title") != null){
-                product.setName(array.getJSONObject(0).getString("title"));
-            }
-
-            //Set the lowest recorded proce offered by the API
-            if(firstItem.getDouble("lowest_recorded_price") != 0.0){
-                product.setName(array.getJSONObject(0).getString("title"));
-            }
-
-            //Set the highest recorded proce offered by the API
-            if(firstItem.getDouble("highest_recorded_price") != 0.0){
-                product.setName(array.getJSONObject(0).getString("title"));
-            }
-
-            //Set the image for the product's thumbnail
-            JSONArray imageArray = firstItem.getJSONArray("images");
-
-            if(imageArray != null){
-                try{
-                    URL url = new URL(imageArray.getString(0));
-                    HttpURLConnection imageConnection = (HttpURLConnection) url.openConnection();
-                    imageConnection.setDoInput(true);
-                    imageConnection.connect();
-                    InputStream input = imageConnection.getInputStream();
-                    Bitmap myBitmap = BitmapFactory.decodeStream(input);
-
-                    product.setImage(myBitmap);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-
-            //TODO: Check in our database for any reviews made for the product
-
-            //Close our InputStream and Buffered reader
-            streamReader.close();
-            br.close();
-
-        } catch (Exception e){
-            Log.d(TAG, "Unable to process the request");
-            e.printStackTrace();
-        }
-
-        //TODO: Add the scanned product to the database under the corresponding user if the name is not null
-
-        return product;
-    }
-
-    protected void onPostExecute(Product product){
-        //TODO: Check if product name != null, if true then launch info activity, else go back to scan activity with a toast
-        String name = product.getName();
-        if(name != null){
-            //Launch info activity
-            Log.v(TAG, "Name not null");
-        }
-        else{
-            //Not sure if necessary, if it fails then it should go back to scan activity
-        }
-    }
-
-
-}
+//public class AsyncFindProduct extends AsyncTask<String, Void, Product> {
+//
+//    private static final String TAG = "AsyncFindProduct";
+//    private static final String REQUEST_METHOD = "GET";
+//    private static final int READ_TIMEOUT = 15000;
+//    private static final int CONNECTION_TIMEOUT = 15000;
+//    private Context mContext;
+//
+//    public AsyncFindProduct(Context context){
+//        this.mContext = context.getApplicationContext();
+//    }
+//
+//    //Maybe implement something in onPreExecute to tell the user we are searching
+//
+//    protected Product doInBackground(String... params){
+//
+//        Product product = new Product();
+//
+//        try{
+//            //Create the connection to the url to upcitemdb first
+//
+//            URL upcItemDbUrl = new URL("https://api.upcitemdb.com/prod/trial/lookup?upc=" + params[0]);
+//            HttpsURLConnection connection = (HttpsURLConnection) upcItemDbUrl.openConnection();
+//
+//            connection.setRequestMethod(REQUEST_METHOD);
+//            connection.setReadTimeout(READ_TIMEOUT);
+//            connection.setConnectTimeout(CONNECTION_TIMEOUT);
+//
+//            connection.connect();
+//
+//            if(connection.getResponseCode() == 200){
+//                Log.v(TAG, "Successful");
+//            }
+//            else{
+//                Log.v(TAG, "RIP");
+//            }
+//            //Create a new InputStreamReader
+//            InputStream streamReader = (connection.getInputStream());
+//
+//            //Get the result from the API call to a string that can be parsed
+//            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//            String content = br.readLine();
+//            Log.v(TAG, "Content: " + content);
+//
+//            //Turn the response string into a JSONObject we may parse
+//            JSONObject object = new JSONObject(content);
+//            Log.v(TAG, "Items: " + object.getString("items"));
+//
+//            //Get the first result from the array of possible results
+//            JSONArray array = object.getJSONArray("items");
+//            Log.v(TAG, "Title " + array.getJSONObject(0).getString("title"));
+//
+//            //Only want to continue object creation if there are results given from the scanning
+//            if(array.length() >= 1) {
+//                //Set the item from the array of results
+//                JSONObject firstItem = array.getJSONObject(0);
+//
+//                //Set the upc of the product
+//                product.setUpc(params[0]);
+//
+//                //Set the name of the product
+//                if (firstItem.getString("title") != null) {
+//                    product.setName(array.getJSONObject(0).getString("title"));
+//                    Log.v(TAG, "Title set");
+//                }
+//
+//                //Set the lowest recorded proce offered by the API
+//                if (firstItem.getDouble("lowest_recorded_price") != 0.0) {
+//                    product.setLowestPrice(firstItem.getDouble("lowest_recorded_price"));
+//                    Log.v(TAG, "Lowest Price Set " + product.getLowestPrice());
+//                }
+//
+//                //Set the highest recorded price offered by the API
+//                if (firstItem.getDouble("highest_recorded_price") != 0.0) {
+//                    product.setHighestPrice(firstItem.getDouble("highest_recorded_price"));
+//                    Log.v(TAG, "Highest Price Set " + firstItem.getDouble("highest_recorded_price"));
+//                }
+//
+//                //Set the image for the product's thumbnail
+//                JSONArray imageArray = firstItem.getJSONArray("images");
+//
+//                if (imageArray != null) {
+//                    try {
+//                        URL url = new URL(imageArray.getString(0));
+//                        HttpURLConnection imageConnection = (HttpURLConnection) url.openConnection();
+//                        imageConnection.setDoInput(true);
+//                        imageConnection.connect();
+//                        InputStream input = imageConnection.getInputStream();
+//                        Bitmap myBitmap = BitmapFactory.decodeStream(input);
+//
+//                        product.setImage(myBitmap);
+//                        Log.v(TAG, "Thumbnail Set " + product.getImage().toString());
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                        return null;
+//                    }
+//                }
+//            }
+//            else{
+//                Log.v(TAG, "No results given by API.");
+//            }
+//
+//            //TODO: Check in our database for any reviews made for the product
+//
+//            //Close our InputStream and Buffered reader
+//            streamReader.close();
+//            br.close();
+//
+//        } catch (Exception e){
+//            Log.d(TAG, "Unable to process the request");
+//            e.printStackTrace();
+//        }
+//
+//        //TODO: Add the scanned product to the database under the corresponding user if the name is not null
+//
+//        return product;
+//    }
+//
+//    protected void onPostExecute(Product product){
+//        //TODO: Check if product name != null, if true then launch info activity, else go back to scan activity with a toast
+//        String name = product.getName();
+//        if(name != null){
+//            //Launch info activity
+//            Log.v(TAG, "Object found, launching product info");
+//        }
+//        else{
+//            //If it fails, we must launch scan activity again
+//            Log.v(TAG, "Object not found, relaunching the scanner");
+//            Intent intent = new Intent(AsyncFindProduct.this, ScanActivity.class);
+//        }
+//    }
+//
+//
+//}
--- app/src/main/java/osu/edu/cashout/fragments/ScanFragment.java
+++ app/src/main/java/osu/edu/cashout/fragments/ScanFragment.java
@@ -4,10 +4,14 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.Gravity;
@@ -24,13 +28,25 @@ import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.zxing.Result;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import osu.edu.cashout.AsyncFindProduct;
+import javax.net.ssl.HttpsURLConnection;
+
 import osu.edu.cashout.activities.AccountActivity;
 import osu.edu.cashout.activities.LoginActivity;
 import osu.edu.cashout.R;
+import osu.edu.cashout.activities.ScanActivity;
+import osu.edu.cashout.dataModels.Product;
 
 //TODO: Implementation of a button to manually type in upc code if camera does not work
 
@@ -70,14 +86,10 @@ public class ScanFragment extends Fragment implements View.OnClickListener{
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(getActivity(), result.getText(), Toast.LENGTH_SHORT).show();
                         Log.v(TAG, result.getText());
-
-                        //TODO: Release the code scanner after finding a result
-                        mCodeScanner.releaseResources();
-
-                        AsyncFindProduct findProduct = new AsyncFindProduct();
+                        AsyncFindProduct findProduct = new AsyncFindProduct(getActivity());
                         findProduct.execute(result.getText());
+                        Log.v(TAG, "Logging finished asynctask?");
                     }
                 });
             }
@@ -170,5 +182,155 @@ public class ScanFragment extends Fragment implements View.OnClickListener{
             Intent accountIntent = new Intent(mContext, AccountActivity.class);
             startActivity(accountIntent);
         }
+    }
+
+
+    /*
+        Use of an inner class so that we may use the FragmentActivity of ScanFragment to print out
+        Toast messages and to launch a new activity based on the result of the API call.
+    */
+    private static class AsyncFindProduct extends AsyncTask<String, Void, Product> {
+
+        private static final String TAG = "AsyncFindProduct";
+        private static final String REQUEST_METHOD = "GET";
+        private static final int READ_TIMEOUT = 15000;
+        private static final int CONNECTION_TIMEOUT = 15000;
+
+        private WeakReference<FragmentActivity> activityReference;
+
+        AsyncFindProduct(FragmentActivity activity){
+            activityReference = new WeakReference<>(activity);
+        }
+
+        //Maybe implement something in onPreExecute to tell the user we are searching
+
+        protected Product doInBackground(String... params){
+
+            Product product = new Product();
+
+            try{
+                //Create the connection to the url to upcitemdb first
+
+                URL upcItemDbUrl = new URL("https://api.upcitemdb.com/prod/trial/lookup?upc=" + params[0]);
+                HttpsURLConnection connection = (HttpsURLConnection) upcItemDbUrl.openConnection();
+
+                connection.setRequestMethod(REQUEST_METHOD);
+                connection.setReadTimeout(READ_TIMEOUT);
+                connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+                connection.connect();
+
+                //If connection to API is successful, we should attempt to get results from the UPC
+                if(connection.getResponseCode() == 200){
+                    Log.v(TAG, "Successful");
+
+                    //Create a new InputStreamReader
+                    InputStream streamReader = (connection.getInputStream());
+
+                    //Get the result from the API call to a string that can be parsed
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String content = br.readLine();
+                    Log.v(TAG, "Content: " + content);
+
+                    //Turn the response string into a JSONObject we may parse
+                    JSONObject object = new JSONObject(content);
+                    Log.v(TAG, "Items: " + object.getString("items"));
+
+                    //Get the first result from the array of possible results
+                    JSONArray array = object.getJSONArray("items");
+                    Log.v(TAG, "Title " + array.getJSONObject(0).getString("title"));
+
+                    //Only want to create an object if there are results given from the scanning, otherwise stop creation
+                    if(array.length() >= 1) {
+                        //Set the item from the array of results
+                        JSONObject firstItem = array.getJSONObject(0);
+
+                        //Set the upc of the product
+                        product.setUpc(params[0]);
+
+                        //Set the name of the product
+                        if (firstItem.getString("title") != null) {
+                            product.setName(array.getJSONObject(0).getString("title"));
+                            Log.v(TAG, "Title set");
+                        }
+
+                        //Set the lowest recorded proce offered by the API
+                        if (firstItem.getDouble("lowest_recorded_price") != 0.0) {
+                            product.setLowestPrice(firstItem.getDouble("lowest_recorded_price"));
+                            Log.v(TAG, "Lowest Price Set " + product.getLowestPrice());
+                        }
+
+                        //Set the highest recorded price offered by the API
+                        if (firstItem.getDouble("highest_recorded_price") != 0.0) {
+                            product.setHighestPrice(firstItem.getDouble("highest_recorded_price"));
+                            Log.v(TAG, "Highest Price Set " + firstItem.getDouble("highest_recorded_price"));
+                        }
+
+                        //Set the image for the product's thumbnail
+                        JSONArray imageArray = firstItem.getJSONArray("images");
+
+                        if (imageArray != null) {
+                            try {
+                                URL url = new URL(imageArray.getString(0));
+                                HttpURLConnection imageConnection = (HttpURLConnection) url.openConnection();
+                                imageConnection.setDoInput(true);
+                                imageConnection.connect();
+                                InputStream input = imageConnection.getInputStream();
+                                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+
+                                product.setImage(myBitmap);
+                                Log.v(TAG, "Thumbnail Set " + product.getImage().toString());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                return null;
+                            }
+                        }
+                    }
+                    //Could not establish connection to the API
+                    else{
+                        Log.v(TAG, "No results given by API.");
+                    }
+                    //TODO: Check in our database for any ratings made for the product and add it to the product we are creating
+
+                    //Close our InputStream and Buffered reader
+                    streamReader.close();
+                    br.close();
+                }
+                else{
+                    Log.v(TAG, "Could not successfully connect to the API.");
+                }
+            } catch (Exception e){
+                Log.d(TAG, "Unable to process the request");
+                e.printStackTrace();
+            }
+
+            //TODO: Add the scanned product to the database under the corresponding user if the name is not null
+
+            return product;
+        }
+
+        protected void onPostExecute(Product product){
+            FragmentActivity referencedActivity = activityReference.get();
+
+            String name = product.getName();
+            if(name != null){
+                //Launch info activity because we found a valid object
+                Log.v(TAG, "Object found, launching product info");
+                Toast.makeText(activityReference.get(), "Name found", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                //If it fails, we must launch scan activity again
+                Log.v(TAG, "Object not found, relaunching the scanner");
+                Toast.makeText(referencedActivity.getApplicationContext(), "Couldn't find that product!",
+                        Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(referencedActivity, ScanActivity.class);
+                //We call finish instead of recreate() so that the camera can resynchronize with the new activity
+                referencedActivity.finish();
+                referencedActivity.startActivity(intent);
+            }
+        }
+
+
     }
 }
