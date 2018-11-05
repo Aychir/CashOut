c5dc0448c2b3797a493c565ab714565fdc9973ae
connorcimino <connorcimino1@gmail.com>
2018-11-05 02:53:56 -0500

    Updated adapter for recyclerview

--- app/src/main/java/osu/edu/cashout/MyAdapter.java
+++ app/src/main/java/osu/edu/cashout/MyAdapter.java
@@ -1,4 +1,40 @@
 package osu.edu.cashout;
 
-public class MyAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] mDataset;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public MyViewHolder(TextView v){
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.view_history_item, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position){
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount(){
+        return mDataset.length;
+    }
+
 }
